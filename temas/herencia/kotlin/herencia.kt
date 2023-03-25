8de311664ae449249f8ba8d5f14d3c3ab8036793
Lucía Atienza Olmo <126182984+lucia-atienza@users.noreply.github.com>
2023-03-25 17:34:47 +0100

    Update herencia.kt

--- temas/herencia/kotlin/herencia.kt
+++ temas/herencia/kotlin/herencia.kt
@@ -1,77 +1,59 @@
-// Clase base que representa un dispositivo de Raspberry Pi
-open class RaspberryPiDevice(val id: Int, val name: String) {
-    fun bootUp() {
-        println("$name $id está arrancando")
-    }
-
-    fun shutDown() {
-        println("$name $id está apagándose")
+open class Casa(val nombre: String, val lema: String, val miembros: Int) {
+    open fun conquistarTierras() {
+        println("$nombre ha conquistado nuevas tierras. ")
     }
 }
 
-// Clase que representa un sensor conectado a un puerto GPIO de la Raspberry Pi
-open class RaspberryPiSensor(id: Int, name: String, val gpioPin: Int) : RaspberryPiDevice(id, name) {
-    open fun readSensorData(): Double {
-        println("$name $id leyendo datos del sensor en el puerto GPIO $gpioPin")
-        return 0.0
+open class CasaNoble(nombre: String, lema: String, miembros: Int, val tierras: Int, var vasallos: Int) : Casa(nombre, lema, miembros) {
+    override fun conquistarTierras() {
+        super.conquistarTierras()
+        println("$nombre ahora posee $tierras acres de tierra. ")
+    }
+    
+    fun nombrarVasallo(casaMenor: CasaMenor) {
+        if (vasallos > 0) {
+            println("$nombre ha nombrado a ${casaMenor.nombre} como su vasallo")
+            vasallos--
+        } else {
+            println("$nombre no tiene suficientes vasallos para nombrar a ${casaMenor.nombre}")
+        }
     }
 }
 
-// Clase que representa un sensor de temperatura conectado a un puerto GPIO de la Raspberry Pi
-class RaspberryPiTemperatureSensor(id: Int, name: String, gpioPin: Int) : RaspberryPiSensor(id, name, gpioPin) {
-    override fun readSensorData(): Double {
-        // Código para leer la temperatura del sensor conectado al puerto GPIO
-        println("$name $id leyendo temperatura del sensor en el puerto GPIO $gpioPin")
-        return 25.0
+class CasaReal(nombre: String, lema: String, miembros: Int, tierras: Int, vasallos: Int, val tronoDeHierro: Boolean) : CasaNoble(nombre, lema, miembros, tierras, vasallos) {
+    override fun conquistarTierras() {
+        super.conquistarTierras()
+        println("$nombre además de tener derecho a nombrar a sus propios vasallos")
     }
-}
-// Clase que representa un actuador conectado a un puerto GPIO de la Raspberry Pi
-open class RaspberryPiActuator(id: Int, name: String, val gpioPin: Int) : RaspberryPiDevice(id, name) {
-    open fun setActuatorState(state: Boolean) {
-        println("$name $id configurando el estado del actuador en el puerto GPIO $gpioPin a $state")
-    }
-}
-
-// Clase que representa un LED conectado a un puerto GPIO de la Raspberry Pi
-class RaspberryPiLED(id: Int, name: String, gpioPin: Int) : RaspberryPiActuator(id, name, gpioPin) {
-    override fun setActuatorState(state: Boolean) {
-        // Código para configurar el estado del LED conectado al puerto GPIO
-        println("$name $id configurando el estado del LED en el puerto GPIO $gpioPin a $state")
+    
+    fun sentarEnElTrono(casaNoble: CasaNoble) {
+        if (tronoDeHierro) {
+            println("$nombre ha sentado a ${casaNoble.nombre} en el Trono de Hierro")
+        } else {
+            println("$nombre no tiene derecho a sentar a ${casaNoble.nombre} en el Trono de Hierro")
+        }
     }
 }
 
-// Clase que representa un relé conectado a un puerto GPIO de la Raspberry Pi
-class RaspberryPiRelay(id: Int, name: String, gpioPin: Int) : RaspberryPiActuator(id, name, gpioPin) {
-    override fun setActuatorState(state: Boolean) {
-        // Código para configurar el estado del relé conectado al puerto GPIO
-        println("$name $id configurando el estado del relé en el puerto GPIO $gpioPin a $state")
+class CasaMenor(nombre: String, lema: String, miembros: Int, val proteccionNoble: CasaNoble) : Casa(nombre, lema, miembros) {
+    fun solicitarProteccion() {
+        println("$nombre ha solicitado protección de ${proteccionNoble.nombre}")
+        proteccionNoble.nombrarVasallo(this)
     }
-}
-
-fun interactWithRaspberryPiDevice(device: RaspberryPiDevice) {
-    device.bootUp()
-    device.shutDown()
-}
-
-fun readRaspberryPiSensorData(sensor: RaspberryPiSensor) {
-    sensor.readSensorData()
-}
-
-fun setRaspberryPiActuatorState(actuator: RaspberryPiActuator, state: Boolean) {
-    actuator.setActuatorState(state)
 }
 
 fun main() {
-    val temperatureSensor = RaspberryPiTemperatureSensor(1, "Sensor de Temperatura", 4)
-    val led = RaspberryPiLED(2, "LED", 17)
-    val relay = RaspberryPiRelay(3, "Relé", 27)
+    
+    val casaTargaryen = CasaReal("Targaryen", "Fuego y sangre", 3, 1000, 10, true)
+    val casaStark = CasaNoble("Stark", "Se acerca el invierno", 5, 500, 5)
+    val casaKarstark = CasaMenor("Karstark", "El sol de invierno", 2, casaStark)
+    
+	val casas: MutableList<Casa> = mutableListOf(casaTargaryen, casaStark,casaKarstark)
+     for (casa in casas) {
+        casa.conquistarTierras()
+    }
 
-    interactWithRaspberryPiDevice(temperatureSensor)
-    readRaspberryPiSensorData(temperatureSensor)
-
-    interactWithRaspberryPiDevice(led)
-    setRaspberryPiActuatorState(led, true)
-
-    interactWithRaspberryPiDevice(relay)
-    setRaspberryPiActuatorState(relay, false)
+    casaTargaryen.sentarEnElTrono(casaStark)
+    casaKarstark.solicitarProteccion()
 }
+
