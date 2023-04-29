3c32815878d085dc2c95f44b12271044abdda294
Lucía Atienza Olmo <126182984+lucia-atienza@users.noreply.github.com>
2023-04-29 12:43:14 +0200

    Delete temas/inyeccion/python directory

--- temas/inyeccion/python/inyeccion/app/models/Actividad.scala
+++
@@ -1,9 +0,0 @@
-package models
-
-import javax.inject.Inject
-
-case class Actividad @Inject() (nombre: String, dificultad: String) {
-  def empezar(): Unit = {
-    println(s"Vamos a empezar la actividad $nombre!")
-  }
-}
--- temas/inyeccion/python/inyeccion/app/models/Equipo.scala
+++
@@ -1,9 +0,0 @@
-package models
-
-import javax.inject.Inject
-
-case class Equipo @Inject()(nombre: String, lider: String) {
-  def presentarse(): Unit = {
-    println(s"Se presenta el equipo $nombre y nuestro líder es $lider. Scouts siempre listos!")
-  }
-}
