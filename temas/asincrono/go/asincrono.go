ba395d8d045c5596bcf072a25fc2bd56cb879cbc
Lucía Atienza Olmo <126182984+lucia-atienza@users.noreply.github.com>
2023-04-29 12:53:36 +0200

    Programación asíncrona con Golang

--- temas/asincrono/go/asincrono.go
+++ temas/asincrono/go/asincrono.go
@@ -1,30 +1,53 @@
+// ejecución online: https://go.dev/play/, https://www.tutorialspoint.com/execute_golang_online.php
 package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
-// Una función que realiza una operación de larga duración y devuelve un entero
-func myFunction() int {
-	time.Sleep(time.Second)
-	return 1
+// recibe una lista de números enteros y devuelve la suma total
+func sum(numbers []int) int {
+	result := 0
+	for _, number := range numbers {
+		result += number
+	}
+	return result
 }
 
-// La función principal que inicia la goroutine y espera el resultado
+// iniciamos las goroutines y espera los resultados
 func main() {
-	// Crear un canal para comunicarse entre la goroutine y la función principal
+	// creamos un canal para comunicarse entre las goroutines y la función principal
 	resultChan := make(chan int)
 
-	// Iniciar la goroutine que ejecutará la función asíncronamente
-	go func() {
-		result := myFunction()
-		// Envía el resultado a través del canal
-		resultChan <- result
-	}()
+	// lista de nums. aleatorios
+	rand.Seed(time.Now().Unix())
+	numbers := make([]int, 1000)
+	for i := 0; i < len(numbers); i++ {
+		numbers[i] = rand.Intn(10)
+	}
 
-	fmt.Println("Esperando el resultado...")
-	// Esperar a que se reciba el resultado a través del canal
-	result := <-resultChan
-	fmt.Println("Resultado:", result+1)
+	// dividimos la anterior lista en 4 para ejecuciones paralelas
+	chunkSize := len(numbers) / 4
+	chunks := make([][]int, 4)
+	for i := 0; i < 4; i++ {
+		chunks[i] = numbers[i*chunkSize : (i+1)*chunkSize]
+	}
+	// inicio goroutines
+	for _, chunk := range chunks {
+		go func(chunk []int) {
+			result := sum(chunk)
+			resultChan <- result
+		}(chunk)
+	}
+
+	// se reciben los datos y se suman
+	total := 0
+	for i := 0; i < 4; i++ {
+		result := <-resultChan
+		total += result
+	}
+
+	fmt.Println("La suma total es:", total)
 }
